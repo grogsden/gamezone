2f8bea418f0aff0a5ba2bec89798dca95efbb7fc
grogsden <brian.stempien@protonmail.com>
2021-04-17 18:51:17 -0500

    added card to the home and review details

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native';
-import { globalStyles } from '../styles/global'
+import { globalStyles } from '../styles/global';
+import Card from '../shared/card';
 
 export default function ReviewDetails({ route }) {
     const { title, body, rating } = route.params;
@@ -8,9 +9,11 @@ export default function ReviewDetails({ route }) {
 
     return (
         <View style={globalStyles.container}>
-            <Text>{ title }</Text>
-            <Text>{ body }</Text>
-            <Text>{ rating }</Text>
+           <Card>
+                <Text>{ title }</Text>
+                <Text>{ body }</Text>
+                <Text>{ rating }</Text>
+           </Card>
         </View>
     )
 }
